9f1e53c4ce9be3ba9a4efc67e084525afa2eb8a2
Timo Salm <tsalm@vmware.com>
2022-05-26 18:20:01 +0200

    first commit

--- workshop/workshop/setup.d/01-dev-namespace.sh
+++ workshop/workshop/setup.d/01-dev-namespace.sh
@@ -6,3 +6,5 @@ echo $CONTAINER_REGISTRY_PASSWORD | docker login $CONTAINER_REGISTRY_HOSTNAME -u
 
 REGISTRY_PASSWORD=$CONTAINER_REGISTRY_PASSWORD kp secret create registry-credentials --registry ${CONTAINER_REGISTRY_HOSTNAME} --registry-user ${CONTAINER_REGISTRY_USERNAME}
 kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "registry-credentials"}, {"name": "tanzu-net-credentials"}]}'
+
+git clone https://github.com/tsalm-pivotal/going-serverless-workshop.git
