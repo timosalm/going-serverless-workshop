9754a127241bb8b8f90d5da42012d931fe9ee40c
Timo Salm <tsalm@vmware.com>
2022-05-26 18:41:18 +0200

    first commit

--- samples/spring-native-accessing-resources/src/main/java/com/example/springnativeaccessingresources/SpringNativeAccessingResourcesApplication.java
+++ samples/spring-native-accessing-resources/src/main/java/com/example/springnativeaccessingresources/SpringNativeAccessingResourcesApplication.java
@@ -2,7 +2,6 @@ package com.example.springnativeaccessingresources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.util.FileCopyUtils;
 import java.io.IOException;
 import java.io.InputStream;
