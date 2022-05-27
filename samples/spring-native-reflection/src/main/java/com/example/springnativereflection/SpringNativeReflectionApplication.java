448d45200a56eac2ae54ec62678b9b789be8a30b
Timo Salm <tsalm@vmware.com>
2022-05-27 12:13:41 +0200

    first commit

--- samples/spring-native-reflection/src/main/java/com/example/springnativereflection/SpringNativeReflectionApplication.java
+++ samples/spring-native-reflection/src/main/java/com/example/springnativereflection/SpringNativeReflectionApplication.java
@@ -2,6 +2,7 @@ package com.example.springnativereflection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 
 @TypeHint(typeNames = {"com.example.springnativereflection.StringReverser"})
 @TypeHint(typeNames = {"com.example.springnativereflection.StringCapitalizer"})
--- workshop/workshop/content/exercises/spring-native-advanced.md
+++ workshop/workshop/content/exercises/spring-native-advanced.md
@@ -23,7 +23,7 @@ You can run the Spring AOT plugin and observe that correct configurations have b
 ```terminal:execute
 command: |
   cd going-serverless-workshop/samples/spring-native-reflection
-  ./mvnw clean package spring-aot:generate
+  ./mvnw clean package -DskipTests  spring-aot:generate
   tree target/generated-sources/spring-aot/src/main/resources/
   cd $HOME
 clear: true
