3440f5d4533a7964c1b10b38ee46e330117b5dde
Timo Salm <52704922+tsalm-vmware@users.noreply.github.com>
2023-05-15 13:20:28 +0200

    Update spring-jvm-knative.sh

--- scripts/spring-jvm-knative.sh
+++ scripts/spring-jvm-knative.sh
@@ -5,16 +5,13 @@ set -u
 set -o pipefail
 
 cd samples/spring-boot-hello-world
-./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=harbor.emea.end2end.link/tap-workshop-examples/spring-boot-hello-world-graalvm-local -DskipTests
-docker push harbor.emea.end2end.link/tap-workshop-examples/spring-boot-hello-world-graalvm-local
-kn service create spring-boot-hello-world --image harbor.emea.end2end.link/tap-workshop-examples/spring-boot-hello-world-graalvm-local --scale-min 1 -n dev-space
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=<container-registry>/tap-workshop-examples/spring-boot-hello-world-graalvm-local -DskipTests
+docker push <container-registry>/tap-workshop-examples/spring-boot-hello-world-graalvm-local
+kn service create spring-boot-hello-world --image <container-registry>/tap-workshop-examples/spring-boot-hello-world-graalvm-local --scale-min 1 -n default
 cd ../..
-curl https://spring-boot-hello-world-dev-space.cnr.emea.end2end.link
-kubectl logs -l app=spring-boot-hello-world-00001 -c user-container -n dev-space | grep "Started HelloWorldApplication"
+curl https://spring-boot-hello-world-default.<env-url>
+kubectl logs -l app=spring-boot-hello-world-00001 -c user-container -n default | grep "Started HelloWorldApplication"
 
 kubectl top pods -l app=spring-boot-hello-world-00001 --containers
 
-hey -z 60s -c 1000 -m GET https://spring-boot-hello-world-graalvm-w02-s012.cnr.emea.end2end.link
-
-
-
+hey -z 60s -c 1000 -m GET https://spring-boot-hello-world-default.<env-url>
