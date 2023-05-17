2be0cacb5a803d001efa7a16e956a793a1651477
Timo Salm <tsalm@vmware.com>
2023-05-17 12:04:30 +0200

    Workshop enhancements

--- workshop/samples/spring-native-class-initialization/src/main/java/com/example/springnativeclassinitialization/SpringNativeClassInitializationApplication.java
+++ workshop/samples/spring-native-class-initialization/src/main/java/com/example/springnativeclassinitialization/SpringNativeClassInitializationApplication.java
@@ -2,15 +2,7 @@ package com.example.springnativeclassinitialization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.InitializationHint;
-import org.springframework.nativex.hint.InitializationTime;
-import org.springframework.nativex.hint.NativeHint;
 
-@NativeHint(
-        initialization = {@InitializationHint(types = {
-                com.example.springnativeclassinitialization.First.class,
-                com.example.springnativeclassinitialization.Second.class
-        }, initTime = InitializationTime.BUILD)})
 @SpringBootApplication
 public class SpringNativeClassInitializationApplication {
 
