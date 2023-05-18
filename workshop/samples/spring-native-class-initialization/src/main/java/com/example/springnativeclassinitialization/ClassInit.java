05db37a9d4e79cedeadfc2340c467eb7514e85d0
Timo Salm <tsalm@vmware.com>
2023-05-18 11:58:28 +0200

    Workshop enhancements

--- workshop/samples/spring-native-class-initialization/src/main/java/com/example/springnativeclassinitialization/ClassInit.java
+++ workshop/samples/spring-native-class-initialization/src/main/java/com/example/springnativeclassinitialization/ClassInit.java
@@ -2,27 +2,15 @@ package com.example.springnativeclassinitialization;
 
 import java.nio.charset.*;
 
-import org.springframework.aot.hint.RuntimeHints;
-import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.stereotype.Component;
 
-@ImportRuntimeHints(ClassInit.ClassInitHints.class)
 @Component
 public class ClassInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
         First.second.printIt();
-    }
-
-    static class ClassInitHints implements RuntimeHintsRegistrar {
-
-        @Override
-        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-            hints.
-        }
     }
 }
 
