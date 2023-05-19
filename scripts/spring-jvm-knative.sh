0e10dc5b57a0bb0e1b163f8925fa878921f7d60a
Timo Salm <tsalm@vmware.com>
2023-05-19 12:45:58 +0200

    Workshop enhancements

--- scripts/spring-jvm-knative.sh
+++ scripts/spring-jvm-knative.sh
@@ -7,7 +7,7 @@ set -o pipefail
 cd samples/spring-boot-hello-world
 ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=<container-registry>/tap-workshop-examples/spring-boot-hello-world-graalvm-local -DskipTests
 docker push <container-registry>/tap-workshop-examples/spring-boot-hello-world-graalvm-local
-kn service create spring-boot-hello-world --image <container-registry>/tap-workshop-examples/spring-boot-hello-world-graalvm-local --scale-min 1 -n default
+kn service create spring-boot-hello-world --image <container-registry>/tap-workshop-examples/spring-boot-hello-world-graalvm-local  -n default
 cd ../..
 curl https://spring-boot-hello-world-default.<env-url>
 kubectl logs -l app=spring-boot-hello-world-00001 -c user-container -n default | grep "Started HelloWorldApplication"
