de2c77302332840b9f4f2092eda4f361e84d90c8
Timo Salm <52704922+timosalm@users.noreply.github.com>
2023-11-21 09:01:03 +0100

    Improvements

--- workshop/setup.d/terminal-setup.sh
+++ workshop/setup.d/terminal-setup.sh
@@ -2,4 +2,10 @@
 set -x
 set +e
 
+
+while (! docker stats --no-stream >/dev/null 2>&1); do
+  echo "Waiting for Docker to launch..."
+  sleep 1
+done
+
 docker pull docker.io/paketobuildpacks/builder:base
